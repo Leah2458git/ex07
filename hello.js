6aac726b6ad1d6ae743aed45ec15488b1074bcc1
dvori5473 <165666022+dvori5473@users.noreply.github.com>
2024-05-06 15:35:51 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,11 +1,9 @@
 const a=()=>{
     console.log("Hello World");
 }
-const b=()=>{
+const bbbbb=()=>{
     console.log("Hello from func B");
 }
-
-
 const ccccccc=()=>{
     console.log("Hello from func C");
 }
